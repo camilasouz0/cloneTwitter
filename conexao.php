aa0f8645563f4a00bb8d13bd47d09d98bea45dc6
Camila Cardoso <camilamaiara@hotmail.com>
2020-02-18 09:56:23 -0300

    18/02/20

--- conexao.php
+++ conexao.php
@@ -1,17 +1,21 @@
 <?php
+/**
+*
+* @return \PDO
+*/
 
-define('SERVER', '10.1.11.152');
-define('BANCO', 'dbtwitter');
-define('SENHA', 'coyote');
-define('USER', 'root');
 
-try {
+function getConnection(){
+    $dsn = 'mysql:host=10.1.11.152; dbtwitter; charset=utf8';
+    $user = 'root';
+    $pass = 'coyote';
  
-$con = new pdo('mysql:host=' . SERVER . ';dbname=' . BANCO, USER, SENHA);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "conexao ok";
-} catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
+    try{
+        $pdo = new PDO($dsn, $user, $pass);
+        return $pdo;
+    } catch(PDOException $e){
+        echo 'Erro: '.$e->getMessage();
+    }
 
+}
 ?>
