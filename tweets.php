352d8e8cc968496c1e9e185d74b4063f753bb25a
Camila Souza <camilamaiara@hotmail.com.br>
2020-03-19 23:28:26 -0300

    19/03/20

--- tweets.php
+++ tweets.php
@@ -1,14 +1,21 @@
 <?php 
 include_once("conexao.php");
+include_once("login.php");//para pegar o id usuario
 //require 'conexao.php';
 $con = getConexao();
 
         
-if(isset($_POST['botao']) && $_POST['botao'] == "enviar"){
+if(isset($_POST['meu_tweet']) && $_POST['meu_tweet'] == "enviar"){
 
-        $tweet = $_POST['tweet'];     
-        $inserirTweet = $con -> prepare("INSERT INTO postTweet (tweet) VALUES (:tweet)");
-        $inserirTweet -> bindValue(':tweet', $tweet);
+        $id = $_SESSION['sessao_id'];   
+        $tweet = $_POST['tweet'];
+        
+        //echo $_SESSION['sessao_id'];//esse é o id do usuario logado
+        
+        $inserirTweet = $con -> prepare("INSERT INTO post (id, tweet) VALUES (:id, :tweet)");
+        $inserirTweet -> bindParam(':id', $id);
+        //$inserirTweet -> bindValue(':idPost', $idPost);
+        $inserirTweet -> bindParam(':tweet', $tweet);
 
         if($inserirTweet -> execute()){
                 header("Refresh:1; url = menu_usuario.php"); //recarrega a página ao enviar tweet
